baa55048e3e47e5075a72c9b7a018a79c859d002
mgirardin <matheus.girardin@gmail.com>
2020-07-16 16:08:36 -0300

    Setup DI framework

--- StockPriceNotifier/Program.cs
+++ StockPriceNotifier/Program.cs
@@ -1,22 +1,40 @@
 ﻿using System;
 using System.IO;
 using CommandDotNet;
+using CommandDotNet.IoC.MicrosoftDependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace StockPriceNotifier
 {
     class Program
-    {
+    {    
+        private static IServiceProvider _serviceProvider;
+
         static void Main(string[] args)
         {
-            BuildAppRunner().Run(args);
+            RegisterServices();
+
+            BuildAppRunner(_serviceProvider).Run(args);
+
+            DisposeServices();
         }
 
-        public static AppRunner<Commands> BuildAppRunner()
+        public static AppRunner<Commands> BuildAppRunner(IServiceProvider serviceProvider)
         {
             AppRunner<Commands> appRunner = new AppRunner<Commands>();
-            appRunner.UseTypoSuggestions();
+            appRunner.UseMicrosoftDependencyInjection(serviceProvider)
+                     .UseTypoSuggestions();
             return appRunner;
+        }
+
+        private static void RegisterServices()
+        {
+            var collection = new ServiceCollection();
+            var config = LoadConfiguration();
+            collection.AddSingleton(config);        
+            collection.AddSingleton<Commands, Commands>();
+            _serviceProvider = collection.BuildServiceProvider();
         }
 
         public static IConfiguration LoadConfiguration()
@@ -26,5 +44,18 @@ namespace StockPriceNotifier
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
             return builder.Build();
         }
+
+        private static void DisposeServices()
+        {
+            if(_serviceProvider == null)
+            {
+                return;
+            }
+            if (_serviceProvider is IDisposable)
+            {
+                ((IDisposable)_serviceProvider).Dispose();
+            }
+        }
+
     }
 }
